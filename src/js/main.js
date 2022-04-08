f480f55b6a0487671cbd6d145ac5ee58aa0e1fba
Alicia Padrón <aliciaapadron@gmail.com>
2022-04-08 16:52:48 +0200

    add version 1 funtional web

--- src/js/main.js
+++ src/js/main.js
@@ -1,140 +1,152 @@
+//VARIABLES GLOBALES//
+
 'use strict';
-//-----------------------------------------constantes y variables
-const listCoctails = document.querySelector('.js_listCoctails');
+
 const listFavoriteCoctails = document.querySelector('.js_listFavoriteCoctails');
-const filteredInput = document.querySelector('.js_inputCoctail');
+const listCoctails = document.querySelector('.js_listCoctails');
+const searchInputTitle = document.querySelector('.js_inputCoctail');
 const buttonSearch = document.querySelector('.js_btnSearch');
-// const buttonReset = document.querySelector('.js_btnReset');
-let coctails = [];
-let favorites = [];
+const buttonReset = document.querySelector('.js_btnReset');
+const buttonResetFav = document.querySelector('.js-button_reset_fav');
+const defaultImage =
+  'https://via.placeholder.com/210x295/ffffff/666666/?text=coctail%20img';
 
-//-----------------------------------------funciones
-//FUNCIÓN PARA ESCUCHAR CADA ITEM DE LA LISTA
-function handleFilter(event) {
-  event.preventDefault();
-  const filteredValue = filteredInput.value;
-  const listFiltered = coctails.filter((coctail) => {
-    return coctail.strDrink.toLowerCase().includes(filteredValue.toLowerCase());
-  });
-  paintCoctails(listFiltered);
-}
-
-//FUNCIÓN PARA SABER SI ES FAVORITO
-function handleCoctail(ev) {
-  const selectedCoctail = ev.currentTarget.id;
-  const objetClicked = coctails.find((coctail) => {
-    return coctail.idDrink === selectedCoctail;
-  });
-  const favoritesFound = favorites.findIndex((fav) => {
-    return fav.idDrink === selectedCoctail;
-  });
-  if (favoritesFound === -1) {
-    favorites.push(objetClicked);
-  } else {
-    favorites.splice(favoritesFound, 1);
-  }
-}
-
-// FUNCIÓN PARA ESCUCHAR LOS EVENTOS DE LOS COCTAILS
-function listenCoctails() {
-  const listCoctails = document.querySelectorAll('.js-coctail');
-  for (const coctailElement of listCoctails) {
-    coctailElement.addEventListener('click', handleCoctail);
-  }
-}
-
-//FUNCIÓN ENCOTRAR LOS FAVORITOS
-function isFavorite(coctail) {
-  const favoriteFound = favorites.find((fav) => {
-    return fav.idDrink === coctail.idDrink;
-  });
-  if (favoriteFound === undefined) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-//FUNCIÓN PARA PINTAR LAS LISTAS DE CÓCTELES
-function paintCoctails(coctails) {
-  let html = '';
-  let favClass = '';
-  if (coctails !== null) {
-    for (const coctail of coctails) {
-      const isFav = isFavorite(coctail);
-      if (isFav) {
-        favClass = 'list-coctail-favorite';
-        html = paintItemList(coctail, favClass, html);
-        listFavoriteCoctails.innerHTML = html;
-      } else {
-        favClass = '';
-        html = paintItemList(coctail, favClass, html);
-
-        listCoctails.innerHTML = html;
-      }
-    }
-  }
-  listenCoctails();
-}
-
-//FUNCIÓN PARA PINTAR LOS ITEMS DE LAS LISTAS
-function paintItemList(item, classCss, html) {
-  html += `<li class="coctail js-coctail ${classCss}" id="${item.idDrink}">`;
-  html += `<h2>${item.strDrink}</h2>`;
-  if (item.strDrinkThumb !== null) {
-    html += `<img class="imgcoctail" src=${item.strDrinkThumb}>`;
-  } else {
-    html += `<img class="imgcoctail" src="https://via.placeholder.com/210x295/ffffff/666666/?text=coctail%20img">`;
-  }
-  html += `</li>`;
-  return html;
-}
-
-// // FUNCIÓN PARA AÑADIR INFORMACIÓN AL LOCAL STORAGE
-// function setInLocalStorage() {
-//   // stringify me permite transformar a string el array de palettes
-//   const stringCoctails = JSON.stringify(coctails);
-//   //añadimos  al localStorage  los datos convertidos en string previamente
-//   localStorage.setItem('coctails', stringCoctails);
-// }
+let data = []; //listado búsqueda
+let dataFav = []; //listado favoritos
 
 //FUNCIÓN PARA HACER EL FETCH CON LA API
-function getFromApi(ev) {
-  let filteredInputValue = filteredInput.value;
-  ev.preventDefault();
+function fetchItems() {
   fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${filteredInputValue}`
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInputTitle.value}`
   )
     .then((response) => response.json())
-    .then((data) => {
-      coctails = data.drinks;
-      paintCoctails(coctails);
-      // setInLocalStorage();
+    .then((dataFromApi) => {
+      data = dataFromApi.drinks;
+      renderAllItems();
     });
 }
-// FUNCIÓN PARA TRAER EL LOCALSTORAGE
-// function getLocalStorage() {
-//   // obtenermos lo que hay en el LS
-//   const localStorageCoctails = localStorage.getItem('coctails');
-//   // siempre que cojo datos del local storage tengo que comprobar si son válidos
-//   // es decir si es la primera vez que entro en la página
-//   if (localStorageCoctails === null) {
-//     // no tengo datos en el local storage, así que llamo al API
-//     getFromApi();
-//   } else {
-//     // sí tengo datos en el local storage, así lo parseo a un array y
-//     const arrayCoctails = JSON.parse(localStorageCoctails);
-//     // lo guardo en la variable global de palettes
-//     coctails = arrayCoctails;
-//     // cada vez que modifico los arrays de palettes o de favorites vuelvo a pintar y a escuchar eventos
-//     paintCoctails(coctails);
-//   }
-// }
-//FUNCIÓN RESET
-// function handleReset() {}
-//---------------------------------------------eventos
-buttonSearch.addEventListener('click', getFromApi);
-filteredInput.addEventListener('click', handleFilter);
-// buttonReset.addEventListener('click', handleReset);
 
-// getLocalStorage();
+//FUNCIÓN PARA CARGAR LA LISTA DEL ITEM BUSCADO
+function renderAllItems() {
+  listCoctails.innerHTML = ''; //pongo la lista vacía
+  if (data !== null) {
+    for (const eachCoctail of data) {
+      renderItem(eachCoctail); //carga la información de cada item/pinta cada li
+    }
+    const coctailLists = document.querySelectorAll('.js-list'); //coge todos los elementos que tenga esa clase
+    for (const eachCoctailList of coctailLists) {
+      eachCoctailList.addEventListener('click', handleCoctailFav);
+    }
+  } else {
+    listCoctails.innerHTML = 'Cóctel no encontrado';
+  }
+}
+
+//FUNCIONES APRA PINTAR EN EL HTML
+
+function renderItem(data) {
+  let imageUrl = data.strDrinkThumb;
+  if (data.strDrinkThumb === null) {
+    imageUrl = defaultImage;
+  }
+  const list = document.createElement('li');
+  list.classList.add('js-list');
+  list.dataset.name = data.strDrink;
+  list.dataset.img = imageUrl;
+  let html = `<img class="js-image imgcoctail" src="${imageUrl}" alt="${data.strDrink}">`;
+  html += `<h3 class="js-card__title">${data.strDrink}</h3>`;
+  list.innerHTML = html;
+  const isFav = dataFav.find((fav) => fav.name === data.strDrink);
+  if (isFav !== undefined) {
+    list.classList.add('js-fav');
+  }
+  listCoctails.appendChild(list); //añado los li al ul
+}
+
+function renderAllItemsFav() {
+  listFavoriteCoctails.innerHTML = ''; //pongo la lista vacía
+
+  for (const eachCoctailFav of dataFav) {
+    renderItemFav(eachCoctailFav);
+  }
+  const botones = document.querySelectorAll('.js-favItem');
+
+  for (const eachboton of botones) {
+    eachboton.addEventListener('click', handleBoton);
+  }
+}
+
+function renderItemFav(dataFav) {
+  const li = document.createElement('li');
+  let imageUrl = dataFav.img;
+
+  if (dataFav.img === null) {
+    imageUrl = defaultImage;
+  }
+  li.classList.add('js-favItem');
+  li.dataset.name = dataFav.name;
+  li.dataset.img = imageUrl;
+  let html = `<img class="js-image imgcoctail" src="${imageUrl}" alt="" placeholder="">`;
+  html += `<h3 class="js-card__title">${dataFav.name}</h3>`;
+  li.innerHTML = html;
+
+  listFavoriteCoctails.appendChild(li);
+}
+
+//FUNCIONES MANEJADORAS
+function handleBoton(event) {
+  console.log(event.currentTarget);
+  console.log(event.currentTarget.dataset);
+}
+//FUNCIÓN QUE SE EJECUTA AL HACER CLICK EN EL BTN BUSCAR
+//SIRVE PARA EJECUTAR LA LLAMADA A LA API
+function handleCoctailTitle(event) {
+  event.preventDefault();
+  fetchItems();
+}
+//FUNCIÓN QUE SE EJECUTA AL HACER CLICK EN UN ITEM DE LA LISTA
+//
+function handleCoctailFav(event) {
+  const existsFav = dataFav.find(
+    (fav) => fav.name === event.currentTarget.dataset.name
+  );
+
+  if (existsFav === undefined) {
+    dataFav.push(event.currentTarget.dataset); //añade un elemento al array
+    event.currentTarget.classList.add('list-coctail-favorite'); //le añade una clase
+    setInLocalStorage(); //lo añade al LocalStorage
+    renderAllItemsFav(); //vuelve a crear la lista de favoritos
+  } else {
+    alert('El cóctel seleccionado ya está en la lista de favoritos'); //si está en la lista
+  }
+}
+
+//FUNCIÓN PARA BORRAR TODOS LOS FAVORITOS
+function handleClickResetAllFav() {
+  dataFav = [];
+  localStorage.clear();
+  renderAllItemsFav();
+}
+
+function setInLocalStorage() {
+  //convertir el array en un objeto JSON y eso se guarda en la memoria del navegador
+  localStorage.setItem('dataLocalStorageFav', JSON.stringify(dataFav));
+}
+
+function getFromLocalStorage() {
+  const savedDataFav = localStorage.getItem('dataLocalStorageFav');
+  if (savedDataFav === null) {
+    dataFav = [];
+  } else {
+    dataFav = JSON.parse(savedDataFav);
+    renderAllItemsFav();
+  }
+}
+
+//función que se ejecuta al entrar en la web
+getFromLocalStorage();
+
+//--------------------------------EVENTOS
+buttonSearch.addEventListener('click', handleCoctailTitle);
+buttonResetFav.addEventListener('click', handleClickResetAllFav);
+buttonReset.addEventListener('click', handleClickResetAllFav);
